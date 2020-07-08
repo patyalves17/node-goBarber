e4558d347ea074d258eb176052c2c106507dd1dc
Paty Alves <patriciaalves10@gmail.com>
2020-07-08 17:22:48 -0300

    created service

---
+++ src/services/CreateAppointment.service.ts
@@ -0,0 +1,39 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider: string;
+
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const AppointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      AppointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: AppointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
